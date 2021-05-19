8983d15f1a821323d3559b47f53347c061c5e5cb
GRACIOUS28 <onesihawu@gmail.com>
2021-05-19 13:12:57 +0200

    assignment2

---
+++ Assignment 2/ProgrammingIsFun.java
@@ -0,0 +1,12 @@
+//ProgrammingIsFun.java
+package com.gmail.onesihawu;
+import javax.swing.*;
+
+public class ProgrammingIsFun
+{
+	public static void main(String[] args) 
+	{
+		String progIsFun = "Programming Is Fun!";
+		JOptionPane.showMessageDialog(null,progIsFun,"Programming Is Fun!",1);		
+	}
+}
